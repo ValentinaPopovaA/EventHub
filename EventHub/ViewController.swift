d6fb4a04e1da8404c5f7f1010f09884ced7528d2
Valentina Popova <info@valentina-popova.ru>
2024-11-16 11:01:28 +0300

    Initial Commit

---
+++ EventHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventHub
+//
+//  Created by Валентина Попова on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
