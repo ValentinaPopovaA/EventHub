bbf131770a512774abe605c405d9019c5faababa
Valentina Popova <info@valentina-popova.ru>
2024-11-18 16:01:44 +0300

    Add ppictures and label

--- EventHub/OnboardingScreen/ViewController.swift
+++ EventHub/OnboardingScreen/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var label = UILabel.makeCustomLabel(text: "Hello", fontSize: 16, textColor: .white, numberOfLines: 0, textAligment: .center)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        ])
     }
 }
 
