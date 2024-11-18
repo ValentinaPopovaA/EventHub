2f85e0328c08e455e30a1da9a13b5b2d4b079771
PavelAbramoff <Pavel.818@yandex.ru>
2024-11-18 12:22:06 -0800

    add_fonts

---
+++ EventHub/Extensions/UIFont + Extencion.swift
@@ -0,0 +1,35 @@
+//
+//  UIFont + Extencion.swift
+//  EventHub
+//
+//  Created by apple on 11/18/24.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static func airbnbCerealWBd() -> UIFont? {
+        return UIFont.init(name: "AirbnbCerealWBd", size: 12)
+    }
+    
+    static func airbnbCerealWBd14() -> UIFont? {
+        return UIFont.init(name: "AirbnbCerealWBd", size: 14)
+    }
+    
+    static func airbnbCerealWBd16() -> UIFont? {
+        return UIFont.init(name: "AirbnbCerealWBd", size: 16)
+    }
+    
+    static func airbnbCerealWBd18() -> UIFont? {
+        return UIFont.init(name: "AirbnbCerealWBd", size: 18)
+    }
+    
+    static func airbnbCerealWBd22() -> UIFont? {
+        return UIFont.init(name: "AirbnbCerealWBd", size: 22)
+    }
+    
+    static func airbnbCerealWBd24() -> UIFont? {
+        return UIFont.init(name: "AirbnbCerealWBd", size: 24)
+    }
+}
