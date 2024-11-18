5db68586f79e1ff544683efb300b73ecda0fac9d
Valentina Popova <info@valentina-popova.ru>
2024-11-18 21:47:51 +0300

    Add UIButton

---
+++ EventHub/Extensions/UIColor+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Ext.swift
+//  EventHub
+//
+//  Created by Валентина Попова on 18.11.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let primaryBlue = UIColor(named: "primaryBlue")
+}
