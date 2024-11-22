37acd6eb220911776619a6f524eb20aa05e71d33
Boogie-Wo0gie <120246769+Boogie-Wo0gie@users.noreply.github.com>
2024-11-22 23:34:00 +0300

    reorg

---
+++ EventHub/AuthorizationScreens/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  EventHub
+//
+//  Created by Максим Загрядский on 22.11.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    
+   
+}
--- EventHub/AuthorizationScreens/LoginViewController.swift
+++ EventHub/AuthorizationScreens/LoginViewController.swift
@@ -9,8 +9,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - UI Components
+    
+    private let headerView = AuthHeaderView(title: "EventHub", subTitle: "Sign in")
+    
+    
+    // MARK: - LifeCycle
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+
+        self.setupUI()
+        self.view.backgroundColor = .systemBackground
+        
     }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        self.view.addSubview(headerView)
+        self.headerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.headerView.topAnchor.constraint(equalTo: self.headerView.topAnchor),
+            self.headerView.leadingAnchor.constraint(equalTo: self.headerView.leadingAnchor),
+            self.headerView.trailingAnchor.constraint(equalTo: self.headerView.trailingAnchor),
+            self.headerView.widthAnchor.constraint(equalToConstant: 270)
+        ])
+        
+        
+    }
+
 }
