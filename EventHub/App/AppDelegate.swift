f60be1a5f64ff7b7c4a86685d8b4dc2c99dad0be
Boogie-Wo0gie <120246769+Boogie-Wo0gie@users.noreply.github.com>
2024-12-01 13:30:39 +0300

    key

--- EventHub/App/AppDelegate.swift
+++ EventHub/App/AppDelegate.swift
@@ -11,12 +11,17 @@ import GoogleSignIn
 import FirebaseAuth
 import IQKeyboardManagerSwift
 
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate  {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
 
+        
+        IQKeyboardManager.shared.isEnabled = true
+        IQKeyboardManager.shared.resignOnTouchOutside = true
+        
         return true
     }
 
--- EventHub/Network/UIImageView.swift
+++ EventHub/Network/UIImageView.swift
@@ -6,19 +6,29 @@
 //
 
 import UIKit
+import Kingfisher
 
 extension UIImageView {
-    func loadImage(from url: String) {
-        guard let imageURL = URL(string: url) else { return }
-        URLSession.shared.dataTask(with: imageURL) { data, _, error in
-            if let error = error {
-                print("Failed to load image: \(error)")
-                return
-            }
-            guard let data = data, let image = UIImage(data: data) else { return }
-            DispatchQueue.main.async {
-                self.image = image
-            }
-        }.resume()
-    }
+    
+    func loadImage(from url: String, placeholder: UIImage? = UIImage(named: "globe")) {
+            guard let imageURL = URL(string: url) else { return }
+            self.kf.setImage(
+                with: imageURL,
+                placeholder: placeholder,
+                options: [.transition(.fade(0.2))]
+            )
+        }
+//    func loadImage(from url: String) {
+//        guard let imageURL = URL(string: url) else { return }
+//        URLSession.shared.dataTask(with: imageURL) { data, _, error in
+//            if let error = error {
+//                print("Failed to load image: \(error)")
+//                return
+//            }
+//            guard let data = data, let image = UIImage(data: data) else { return }
+//            DispatchQueue.main.async {
+//                self.image = image
+//            }
+//        }.resume()
+//    }
 }
