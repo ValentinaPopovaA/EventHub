e72fd4e223da5fa94a56a1ae3bc8b6f80584a04a
Boogie-Wo0gie <120246769+Boogie-Wo0gie@users.noreply.github.com>
2024-12-02 19:48:40 +0300

    map

--- EventHub/MapScreen/MapViewController.swift
+++ EventHub/MapScreen/MapViewController.swift
@@ -8,11 +8,27 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Screen_3")
+        imageView.contentMode = .scaleAspectFit //
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        view.addSubview(backgroundImageView)
+            
+            NSLayoutConstraint.activate([
+                backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+                backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
     }
     
 
